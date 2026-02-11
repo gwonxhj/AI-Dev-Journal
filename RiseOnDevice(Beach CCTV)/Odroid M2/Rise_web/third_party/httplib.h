8866b70d22e3c783d2669a93b11572d3fd00e32a
hyeokjun32 <ksjm0417@naver.com>
2026-02-11 14:32:20 +0900

    Update httplib.h

--- RiseOnDevice(Beach CCTV)/Odroid M2/Rise_web/third_party/httplib.h
+++ RiseOnDevice(Beach CCTV)/Odroid M2/Rise_web/third_party/httplib.h
@@ -1,11 +1,11 @@
 wget https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
 
 이후 app_server.cpp에서
-  ```text
+'''text
 #include "httplib.h"
-```
+'''
 
 CMakeLists.txt에
-```text
+'''text
 include_directories(${CMAKE_SOURCE_DIR}/third_party)
-```
+'''
